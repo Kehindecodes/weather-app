5fc3343e5d70734df13768e7dbd55d6e84cbf489
Oki Kehinde <olamiposi.oki@gmail.com>
2020-05-22 15:02:35 +0100

    updated

--- js/weather.js
+++ js/weather.js
@@ -7,7 +7,7 @@ class Weather{
     }
     //fetch weather from API
     async getWeather(){
-        const response = await fetch (`http://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=${this.apiKey}`);
+        const response = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=${this.apiKey}`);
         const responseData = await  response.json()
         return responseData;
     }
